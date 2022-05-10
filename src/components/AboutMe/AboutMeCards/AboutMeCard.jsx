fd2c3c0fd173490b779d5fd1286af9ea602d7330
Kasymov Marsel <marselkasymov23@gmail.com>
2022-05-10 16:34:35 +0600

    fix

--- src/components/AboutMe/AboutMeCards/AboutMeCard.jsx
+++ src/components/AboutMe/AboutMeCards/AboutMeCard.jsx
@@ -44,7 +44,7 @@ export default function AboutMeCards({value}) {
                             <h4>
                                 <FormattedMessage id='ITEducationText'/>
                             </h4>
-                             <a target='_blank' rel='noreferrer' href="https://codify.home.kg/"> {value === 'RU-RU' ? 'Курсы' : null} Codify {value === 'en-US' ? 'Courses' : null}</a><FormattedMessage id='ITEducation'/>
+                             <a target='_blank' rel='noreferrer' href="https://www.codifylab.com/"> {value === 'RU-RU' ? 'Курсы' : null} Codify {value === 'en-US' ? 'Courses' : null}</a><FormattedMessage id='ITEducation'/>
                         </li>
                     </ul>
             </div>
@@ -68,7 +68,7 @@ export default function AboutMeCards({value}) {
                             <h4>
                                 <FormattedMessage id='ITEducationText'/>
                             </h4>
-                             <a target='_blank' rel='noreferrer' href="https://codify.home.kg/"> {value === 'RU-RU' ? 'Курсы' : null} Codify {value === 'en-US' ? 'Courses' : null}</a><FormattedMessage id='ITEducation'/>
+                             <a target='_blank' rel='noreferrer' href="https://www.codifylab.com/"> {value === 'RU-RU' ? 'Курсы' : null} Codify {value === 'en-US' ? 'Courses' : null}</a><FormattedMessage id='ITEducation'/>
                         </li>
                     </ul>
             </div>
